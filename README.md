491f2fb84da20bb3d93db417bbcc24b292ed93b6
Yeiker <yeiker.vazquez@klarna.com>
2017-10-15 21:06:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# roman-number-parser
+
+Steps for creating the parser:
+
+1. Jasmine example project
+2. Create lib and tests
---
+++ spec/roman_number/RomanNumberParserSpec.js
@@ -0,0 +1,38 @@
+'use strict';
+describe('RomanNumberParser', function () {
+    var subject = require('../../lib/RomanNumberParser');
+
+    describe('parse', function () {
+        var testCases = [
+            {
+                arabicNumber: 10000,
+                result: '̅X'
+            },
+            {
+                arabicNumber: 8000,
+                result: '̅VMMM'
+            },
+            {
+                arabicNumber: 9014,
+                result: 'M̅XXIV'
+            },
+            {
+                arabicNumber: 9744,
+                result: 'M̅XDCCXLIV'
+            },
+            {
+                arabicNumber: 0,
+                result: 'N'
+            }
+        ];
+
+        testCases.forEach(function (testCase) {
+            var arabicNumber = testCase.arabicNumber;
+            var result = testCase.result;
+
+            it('when the number ' + arabicNumber + ' is passed it should return ' + result, function () {
+                expect(subject.parse(arabicNumber)).toBe(result);
+            });
+        });
+    });
+});
